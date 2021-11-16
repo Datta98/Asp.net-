f7eca0df7365e646aad33172746726be0db59be1
Datta98 <56407906+Datta98@users.noreply.github.com>
2021-11-16 17:44:21 +0530

    UserControl

---
+++ AspPrograms/UserControlExample/Demo.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Demo.aspx.cs" Inherits="UserControlExample.Demo" %>
+<%@ Register Src="~/WebUserControl1.ascx" TagPrefix="uc1" TagName="WebUserControl1" %>  
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <uc1:WebUserControl1 runat="server" id="webUC"  />           
+        </div>
+    </form>
+</body>
+</html>
---
+++ AspPrograms/UserControlExample/WebUserControl1.ascx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+
+namespace UserControlExample
+{
+    public partial class WebUserControl1 : System.Web.UI.UserControl
+    {
+        private string _header;
+        public string Header
+        {
+            get { return _header; }
+            set { _header = value; }
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            {
+                lblMessage.Text = _header;
+            }
+        }
+    }
+}
